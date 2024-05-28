c7d00ff1f1aebb5043e85e3e6d7e5395f427c699
Charlie Flux <mail@charlieflux.com>
2024-05-28 10:18:06 +0100

    .gitignore is now working

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cflux/Documents/Code/6502Emulator/6502Emulator
-# Build directory: /home/cflux/Documents/Code/6502Emulator/6502Emulator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("tests")
